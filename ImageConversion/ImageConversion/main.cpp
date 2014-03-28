285ed0397c253ee0dc9ebaf4efbb2d60411922e7
CoolestNerdIII <basketball4lyfe2@gmail.com>
2014-03-28 09:45:11 -0400

    Beginning of the ImageConversion Program that I am going to attempt in cpp, using a combo of tesseract and opencv

---
+++ ImageConversion/ImageConversion/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ImageConversion
+//
+//  Created by Paul Wilson on 3/28/14.
+//  Copyright (c) 2014 Paul Wilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
